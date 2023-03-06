60c3338c329e6571497272824114a1de8a8a7fd7
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-03-07 10:55:05 +1100

    created check answer function

--- script.js
+++ script.js
@@ -1,53 +1,104 @@
 var timeEl = document.getElementById("timer");
 var startBtn = document.getElementById("start-button");
+var heading = document.querySelector("h1");
+var btnDiv = document.querySelector("div");
 
+// creates a button
 
-//  When start button is pressed a timeer starts
+var btnCreate = document.createElement("button");
 
+// 1  When start button is pressed a timer starts
 
-
-console.log(timeEl, "time element")
-
-var secondsLeft = 60;
-
+let secondsLeft = 20;
 
 function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time Left: " + secondsLeft;
 
+    if (secondsLeft <= 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // also run future function to display game over
+    }
+  }, 1000);
+}
 
-   
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = "Time Left: " + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // also run future function to display game over
-    
-      }
-  
-    }, 1000);
-  };
+startBtn.addEventListener("click", startQuiz);
 
+function startQuiz() {
+  setTime();
+  questionSet();
+}
 
-  startBtn.addEventListener("click", function() {
-    console.log( "You clicked the start button");
+//  2 Same time the timer starts the page changes with a question and set of answers
 
-    setTime();
+var firstQuestion = {
+  questionTitle: "this is a sample question 1",
 
+  questions: ["what is a", "what is b", "what is c", "what is d"],
+
+  correctAnswer: ["what is b"],
+};
+
+function questionSet() {
+  heading.innerText = firstQuestion.questionTitle;
+
+  startBtn.remove();
+
+  for (let i = 0; i < firstQuestion.questions.length; i++) {
+    let newButton = document.createElement("button");
+    newButton.textContent = firstQuestion.questions[i];
+    btnDiv.appendChild(newButton);
+    CheckAnswer(firstQuestion, newButton);
+  }
+}
+
+// Button Listner function to see if answer is correct or wrong
+
+let userScore = 0;
+
+function CheckAnswer(firstQuestion, newButton, secondsLeft) {
+  newButton.addEventListener("click", function () {
+    const buttonText = newButton.textContent;
+
+    if (buttonText == firstQuestion.correctAnswer) {
+      userScore++;
+      console.log("You have selected the correct answer ");
+
+      console.log("user score is " + userScore);
+    } else buttonText != firstQuestion.correctAnswer;
+
+    {
+      console.log("you clicked the wrong answer!");
+      secondsLeft = secondsLeft - 5;
+      console.log("seconds left after wrong answer " + secondsLeft);
+    }
   });
+}
 
+//  3 when an answer is seleted presented with another answer
 
-// Same time the timer starts the page changes with a question and set of answers
+// 4 if i click the wrong answer time is removed from the timer
 
-// when an answer is seleted presented with another answer
+/*function with if statments that will change timerInterval funciton
 
-// if i click the wrong answer time is removed from the timer
+*/
 
-// when all questions are answered or the timer reaches zero the game is over
+//  5 when all questions are answered or the timer reaches zero the game is over
 
+/*
+function for when all questions are answered or secondsLeft = 0 presented with gameover screen and..
+*/
 
-//Once quiz over i can save my initals and score 
+// 6 Once quiz over i can save my initals and score
 
+/*
 
+form to enter initals 
 
+save score to local storage
+
+then retrieve and make visible on highscore screen 
+
+*/
